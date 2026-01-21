9756917dd452ec351e41d3304c267f1df5f18e02
无损平方集团 <jingyuwang51@gmail.com>
2026-01-21 22:16:01 +0800

    first commit

--- CFG主程序.bat
+++ CFG主程序.bat
@@ -1,2 +1,4 @@
-cd extra\application
-start Autumn.exe
+@echo off
+chcp 65001 > nul
+cd library\execute
+start Listener.exe
---
+++ C_申请测试版访问权限.bat
@@ -0,0 +1,5 @@
+@echo off
+chcp 65001>nul
+cd library\execute
+echo --请联系管理员并提供申请--
+AccessRequest.exe
